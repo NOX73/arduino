6c31d3211712788dffa0cdb76069e38304f7798f
Rozhnov Alexandr <nox73@ya.ru>
2014-08-16 20:46:51 +0400

    Apply Functor like a boss %).

---
+++ home_server/src/Arduino.hs
@@ -0,0 +1,28 @@
+module Arduino (
+  devicePath
+) where
+
+import System.Directory (getDirectoryContents)
+import Data.List (isInfixOf)
+import Control.Monad (filterM)
+import Data.Functor
+
+type DevicePath = String
+
+isTTY :: DevicePath -> Bool
+isTTY = isInfixOf "tty"
+
+isUSB :: DevicePath -> Bool
+isUSB = isInfixOf "Blu"
+
+isDevice :: DevicePath -> Bool
+isDevice path = isTTY path && isUSB path
+
+allDevicePaths :: IO [DevicePath]
+allDevicePaths = getDirectoryContents "/dev/"
+
+devicePaths :: IO [DevicePath]
+devicePaths = filter isDevice <$> allDevicePaths
+
+devicePath :: IO DevicePath
+devicePath = head <$> devicePaths
--- home_server/src/Main.hs
+++ home_server/src/Main.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import Arduino
+import Data.String
 
-main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!!!"
+main = do
+
+  path <- Arduino.devicePath
+
+  scotty 3000 $ do
+    get "/" $ do
+      html $ (fromString path)
