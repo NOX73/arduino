4e45ed6d3e99840cebff0422dbee5fe0e2330a3b
Rozhnov Alexandr <nox73@ya.ru>
2014-09-24 00:15:50 +0400

    Go router pack

---
+++ go_router/go_router.go
@@ -0,0 +1,59 @@
+package go_router
+
+import (
+  "github.com/tarm/goserial"
+  "io"
+  "encoding/binary"
+)
+
+const (
+  SerialBaud = 115200
+)
+
+type Router interface {
+  Read() ([]byte, error)
+  Write([]byte) error
+}
+
+func OpenDevice(path string) (Router, error) {
+  c := &serial.Config{Name: path, Baud: SerialBaud}
+  s, err := serial.OpenPort(c)
+
+  if(err != nil) {
+    return nil, err
+  }
+
+  return &router{s}, nil
+}
+
+type router struct {
+  serial io.ReadWriteCloser
+}
+
+func (r *router) Read () ([]byte, error) {
+  var err error;
+  var length int32;
+
+  err = binary.Read(r.serial, binary.LittleEndian, length);
+  if err != nil {return nil, err}
+
+  pack := make([]byte, length);
+
+  err = binary.Read(r.serial, binary.LittleEndian, pack);
+  if err != nil {return nil, err}
+
+  return pack, nil;
+}
+
+func (r *router) Write (buff []byte) error {
+  var err error;
+  var length int32 = int32(len(buff));
+
+  err = binary.Write(r.serial, binary.LittleEndian, length);
+  if err != nil {return err}
+
+  err = binary.Write(r.serial, binary.LittleEndian, buff);
+  if err != nil {return err}
+
+  return nil;
+}
